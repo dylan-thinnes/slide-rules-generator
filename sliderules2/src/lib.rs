b581cbffc8f8c378416cbada4a34d082da2faffb
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-06-21 20:59:41 +0100

    Reorg main/external function bindings, add comments

--- sliderules2/src/lib.rs
+++ sliderules2/src/lib.rs
@@ -63,15 +63,16 @@ pub enum Show {
 
 impl Default for Show { fn default() -> Self { Show::Display } }
 
+// Main + External function bindings
+#[wasm_bindgen]
+pub fn main() {
+    log("V2 WASM started.");
+}
+
 #[wasm_bindgen]
 extern "C" {
     // Use `js_namespace` here to bind `console.log(..)` instead of just
     // `log(..)`
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn main() {
-    log("V2 WASM started.");
 }
