f3a6397dd75ceffe2ce1b33fea7f2e48f5cf2b80
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-06-21 13:25:55 +0100

    Start fresh sliderules Rust/WASM attempt

---
+++ sliderules2/src/lib.rs
@@ -0,0 +1,14 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn main() {
+    log("V2 WASM started.");
+}
---
+++ sliderules2/www/index.ts
@@ -0,0 +1,3 @@
+import * as wasm from "sliderules";
+
+wasm.main();
