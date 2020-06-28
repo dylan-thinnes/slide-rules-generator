30fb3ffa99d09a86a49cc0cdd59ff39dee6cb52f
Dylan Thinnes <dylan.thinnes@protonmail.com>
2020-06-28 13:58:30 +0100

    Add support for Preact build

--- sliderules2/www/webpack.config.js
+++ sliderules2/www/webpack.config.js
@@ -23,4 +23,12 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin(['index.html'])
   ],
+  resolve: {
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+     // Must be below test-utils
+    },
+  }
 };
