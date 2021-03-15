afd65b0cbb6afaaff4ccd63af556d88cc0acc76b
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-03-15 21:51:19 +0000

    Add pre/post, log/ll transforms, improve tick metadata, etc.

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -29,6 +29,7 @@ import qualified Diagrams.Prelude as D
 import qualified Diagrams.Backend.SVG as D
 import qualified Diagrams.Backend.SVG.CmdLine as D
 import qualified Diagrams.TwoD.Vector as D
+import qualified Diagrams.TwoD.Text as D
 
 import qualified Data.Sequence as S
 import Text.Printf
@@ -45,6 +46,8 @@ import Control.Lens.TH
 import Control.Monad.Extra
 
 import Data.Foldable (toList)
+import System.IO.Unsafe
+import System.Random
 
 import Debug.Trace
 
@@ -54,170 +57,213 @@ main = error "hmm"
 dunit :: Applicative m => m ()
 dunit = pure ()
 
-elseDef :: Applicative m => Bool -> a -> m a -> m a
-elseDef b def action = if b then action else pure def
+elseUnit :: Monad m => Bool -> m a -> m ()
+elseUnit b action = if b then action >> dunit else dunit
 
-elseUnit :: Applicative m => Bool -> m () -> m ()
-elseUnit b = elseDef b ()
-
-leftEndo :: [Endo a] -> a -> a
-leftEndo = appEndo . foldl (<>) mempty
-
---example = lwG 0.05 . mconcat . map fromOffsets
---         $ [ [r *^ e (r @@ rad)]
---           | r <- [33 * tau/32, 34 * tau/32 .. 2 * tau]
---           ]
+e :: Double
+e = sum $ map (recip . fromIntegral . fac) [0..17]
+    where
+        fac :: Integer -> Integer
+        fac n = product ([2..n] :: [Integer])
 
 -- Annotations
 data Ann t = Ann { ann :: String, val :: t }
 instance Show (Ann t) where
     show (Ann s _) = s
 
--- Point generators
-data PG
-  = Single Double
-  | Join (S.Seq PG) -- combine tick generators, superimpose
-  | IterateN Int (Ann (Int -> Double -> Double)) PG
-  | OffsetN Int Double PG
-  | PowN Int Double PG
-  | PowNCap Int Double PG
-  | ScaleN Double PG
-  | OffsetFrom PG (Ann (Double -> PG))
-  deriving Show
-
---run :: PG -> S.Seq Double
---run (Single x) = [x]
---run (Join xs) = foldMap run xs
---run (IterateN n f gen) = do
---    i <- [0..n-1]
---    x <- run gen
---    pure $ val f i x
---run (OffsetN n step gen) =
---    let f = \i x -> x + step * fromIntegral i
---     in run $ IterateN n (Ann "OffsetN" f) gen
---run (PowN n base gen) =
---    let f = \p x -> x * base ** fromIntegral p
---     in run $ IterateN n (Ann "PowN" f) gen
---run (PowNCap n base gen) =
---    run $ Join [PowN n base gen, Single $ base ** fromIntegral n]
---run (ScaleN factor gen) =
---    (factor *) <$> run gen
---run (OffsetFrom gen f) = do
---    x <- run gen
---    [x] <> run (val f x)
---
---pg1 = OffsetN 10 1 (Single 1)
-
 data Transform1
-    = Translate { _gtrOffset :: Double, _gtrScale :: Double }
+    = Offset Double
+    | Scale Double
+    | Log Double
+    | LogLog Double
     deriving Show
 
-type GenTrans = Transform1
 newtype Transform = Transform [Transform1]
+    deriving Show
+
+runGenTransform :: Transform -> Double -> Double
+runGenTransform (Transform ones) = appEndo $ foldMap (Endo . runGenTransform1) $ reverse ones
 
 runGenTransform1 :: Transform1 -> Double -> Double
-runGenTransform1 Translate {..} x = _gtrOffset + _gtrScale * x
-runGenTransform :: Transform -> Double -> Double
-runGenTransform (Transform ones) = leftEndo (map (Endo . runGenTransform1) ones)
+runGenTransform1 (Offset offset) x = offset + x
+runGenTransform1 (Scale scale) x = scale * x
+runGenTransform1 (Log base) x = logBase base x
 
 pushTransform :: Transform1 -> Transform -> Transform
 pushTransform t1 (Transform ts) = Transform (t1 : ts)
 
-popTransform :: Transform -> Transform
-popTransform (Transform []) = Transform []
-popTransform (Transform (x:xs)) = Transform xs
+data Tick meta = Tick { prePos :: Double, postPos :: Double, tickMeta :: meta }
+    deriving (Show)
 
-transform :: GenTrans -> GenTrans -> GenTrans
-transform sub original =
-    Translate
-        { _gtrOffset = _gtrOffset original +  _gtrScale original * _gtrOffset sub
-        , _gtrScale = _gtrScale original * _gtrScale sub
-        }
-
-data GenState meta = GenState { _currTrans :: GenTrans, _out :: S.Seq (Double, meta) }
+data GenState meta = GenState
+    { _preTrans :: Transform
+    , _postTrans :: Transform
+    , _out :: S.Seq (Tick meta)
+    }
     deriving (Show)
 
 makeLenses ''GenState
 
 type Gen meta = ListT (State (GenState meta))
 runGen :: Gen meta a -> GenState meta
-runGen gen = execState (runListT gen) (GenState (Translate 0 1) [])
+runGen gen = execState (runListT gen) (GenState (Transform []) (Transform []) [])
 
 list :: [a] -> Gen meta a
 list xs = ListT $ pure xs
 
-save :: meta -> Double -> Gen meta ()
-save meta x = do
-    Translate {..} <- gets _currTrans
-    let res = _gtrOffset + _gtrScale * x
-    out <>= [(res, meta)]
+preTransformG, postTransformG :: Double -> Gen meta Double
+preTransformG x = gets (runGenTransform . _preTrans) <&> ($ x)
+postTransformG x = gets (runGenTransform . _postTrans) <&> ($ x)
+
+transformG :: Double -> Gen meta (Double, Double)
+transformG x = do
+    pre <- preTransformG x
+    post <- postTransformG pre
+    pure (pre, post)
+
+measureG :: Double -> Double -> Gen meta Double
+measureG a b = do
+    (_, a') <- transformG a
+    (_, b') <- transformG b
+    pure $ b' - a'
+
+save :: meta -> Double -> Gen meta Double
+save tickMeta x = do
+    (prePos, postPos) <- transformG x
+    out <>= [Tick {..}]
+    pure prePos
+
+savePre :: (Double -> meta) -> Double -> Gen meta Double
+savePre tickMetaF x = do
+    tickMeta <- tickMetaF <$> preTransformG x
+    save tickMeta x
 
 zoom :: Double -> Double -> Gen meta a -> Gen meta a
-zoom o s = zoomG (Translate o s)
+zoom o s = offset o . scale s
 
 offset, scale :: Double -> Gen meta a -> Gen meta a
-offset o = zoom o 1
-scale = zoom 0
+offset offset = preZoomG (Offset offset)
+scale scale = preZoomG (Scale scale)
+logGen base = preZoomG (Log base)
 
-zoomG :: GenTrans -> Gen meta a -> Gen meta a
-zoomG transformation gen = do
-    parentState <- gets _currTrans
-    currTrans %= transform transformation
+preZoomG, postZoomG :: Transform1 -> Gen meta a -> Gen meta a
+preZoomG transformation gen = do
+    oldState <- use preTrans
+    preTrans %= pushTransform transformation
     a <- gen
-    currTrans .= parentState
+    preTrans .= oldState
+    pure a
+postZoomG transformation gen = do
+    oldState <- use postTrans
+    postTrans %= pushTransform transformation
+    a <- gen
+    postTrans .= oldState
     pure a
 
-data Meta = Meta { height :: Double, mtext :: Maybe String }
+data TextPosition = Above Double | Below Double | Alongside Double
     deriving Show
-meta :: Double -> String -> Meta
-meta h t = Meta h (Just t)
-metaN :: Double -> Meta
-metaN h = Meta h Nothing
+
+data Meta = Meta { _start :: Double, _end :: Double, _mtext :: Maybe (TextPosition, Double, String) }
+    deriving Show
+makeLenses ''Meta
+
+defMeta :: Meta
+defMeta = Meta { _start = 0, _end = 1, _mtext = Nothing }
+
+meta :: Double -> Double -> Maybe String -> Meta
+meta s e mt = Meta s e ((Above 0, 0.3,) <$> mt)
+
+meta0 :: Double -> Maybe String -> Meta
+meta0 = meta 0
+meta0J :: Double -> String -> Meta
+meta0J h t = meta0 h (Just t)
+meta0N :: Double -> Meta
+meta0N h = meta0 h Nothing
 
 powC :: Int -> Gen Meta ()
-powC n = do
-    save (meta 1 "1") (10 ** fromIntegral n)
-    p <- list [0..n-1]
-    zoom 0 (10 ** fromIntegral p) c
+powC n =
+    postZoomG (Log $ 10 ** fromIntegral n) $ do
+        save (meta0J 1 "1") (10 ** fromIntegral n)
+        p <- list [0..n-1]
+        postZoomG (Scale $ 10 ** fromIntegral p) $ do
+            logScale
 
-c :: Gen Meta ()
-c = do
-    save (meta 0.1 "π") pi
+logScale :: Gen Meta ()
+logScale = do
+    save (Meta 0.6 0.7 $ Just (Above (-0.05), 0.25, "π")) pi
+    save (Meta 0.6 0.7 $ Just (Above (-0.05), 0.25, "e")) e
     x <- list [1..9]
-    save (meta 1 (show x)) (fromIntegral x)
-    offset (fromIntegral x) $
-        if | x <= 3    -> partition [(2,0.7), (5,0.5)]
-           | x <= 6    -> partition [(5,0.5)]
-           | x <= 9    -> partition [(2,0.5)]
-           | otherwise -> pure ()
+    save (meta0J 1 (show x)) (fromIntegral x)
+    offset (fromIntegral x) $ tryPartitions 0.0025 (False, False)
+        [[(2, const $ Meta 0 0.7 $ Just (Above (-0.05), 0.25, ".5")), (5, const $ meta0N 0.5), (5, const $ meta0N 0.25)]
+        ,[(2, const $ Meta 0 0.7 $ Just (Above (-0.05), 0.25, ".5")), (5, const $ meta0N 0.5), (2, const $ meta0N 0.3)]
+        ,[(2, const $ Meta 0 0.7 $ Just (Above (-0.05), 0.25, ".5")), (5, const $ meta0N 0.5)]
+        ,[(5, const $ meta0N 0.5)]
+        ,[(2, const $ meta0N 0.5),(2, const $ meta0N 0.3)]
+        ,[(2, const $ meta0N 0.5)]
+        ]
 
-partition :: [(Int, Double)] -> Gen Meta ()
-partition [] = pure ()
-partition (curr:rest) = do
-    let (n, hgt) = curr
+partition :: (Bool, Bool) -> [(Integer, Double -> Meta)] -> Gen Meta ()
+partition (tickStart, tickEnd) [] = pure ()
+partition (tickStart, tickEnd) (curr:rest) = do
+    let (n, metaF) = curr
+    elseUnit tickEnd $ savePre metaF (fromIntegral n)
     x <- list [0..n-1]
     zoom 0 (1 / fromIntegral n) $ do
-        elseUnit (x /= 0) $ save (metaN hgt) (fromIntegral x)
-        zoom (fromIntegral x) 1 $ partition rest
+        elseUnit (tickStart || x /= 0) $ savePre metaF (fromIntegral x)
+        zoom (fromIntegral x) 1 $ partition (False, False) rest
+
+tryPartitions :: Double -> (Bool, Bool) -> [[(Integer, Double -> Meta)]] -> Gen Meta ()
+tryPartitions threshold startEnd [] = pure ()
+tryPartitions threshold startEnd (partitionSpec:specs) = do
+    minPartitionWidth <- minPartitionWidth partitionSpec
+    if minPartitionWidth > threshold
+       then partition startEnd partitionSpec
+       else tryPartitions threshold startEnd specs
+
+minPartitionWidth :: [(Integer, Double -> Meta)] -> Gen meta Double
+minPartitionWidth xs = do
+    let sliceWidth = product $ map fst xs
+        b = 1
+        a = b - recip (fromIntegral sliceWidth)
+    measureG a b
 
 -- Rendering
-total = D.bgFrame 0.1 D.white $ D.vcat [a]
+total :: D.Diagram D.B
+total = D.bgFrame 0.025 D.white $ D.vsep 0.02 [a, b, c]
 
-a :: D.Diagram D.B
-a = foldMap (mkTick False) $ toList $ _out $ runGen $ powC 4
+a, b, c :: D.Diagram D.B
+a = foldMap (renderTick False) $ toList $ _out $ runGen $ powC 1
+b = foldMap (renderTick False) $ toList $ _out $ runGen $ powC 2
+c = foldMap (renderTick False) $ toList $ _out $ runGen $ powC 3
 
-mkTick :: Bool -> (Double, Meta) -> D.Diagram D.B
-mkTick above (inp, meta) =
-    let x = logBase 1000 inp
-        origTick = laserline [D.r2 (0, 0.04 * height meta)]
+renderTick :: Bool -> Tick Meta -> D.Diagram D.B
+renderTick above Tick { prePos, postPos, tickMeta = meta } =
+    let tickVec   = D.r2 (0, 0.04 * (meta^.end - meta^.start))
+        tickStart = D.r2 (0, 0.04 * meta^.start)
+        tickEnd   = D.r2 (0, 0.04 * meta^.end)
+        origTick = laserline [tickVec]
+                 & D.translate tickStart
         withLabel =
-            case mtext meta of
+            case meta^.mtext  of
               Nothing -> origTick
-              Just text ->
-                  let label
-                        = D.alignedText 0.5 (if above then 0.0 else 1.0) text
-                        & D.fontSizeL 0.015 & D.fc D.black & D.font "monospace"
-                   in D.vsep 0.005 $ (if above then id else reverse) [label, origTick]
-     in D.translate (D.r2 (x, 0)) withLabel
+              Just (position, fontSize, text) ->
+                  let labelOffset =
+                          case position of
+                            Above spacing     -> tickEnd + D.r2 (0, 0.04 * spacing)
+                            Below spacing     -> tickStart - D.r2 (0, 0.04 * spacing)
+                            Alongside spacing -> tickEnd + D.r2 (spacing, 0)
+                      (labelAlignmentX, labelAlignmentY) =
+                          case position of
+                            Above _     -> (0.5, 0)
+                            Below _     -> (0.5, 1)
+                            Alongside _ -> (0, 0.5)
+                      label
+                        = D.alignedText labelAlignmentX labelAlignmentY text
+                        & D.fontSizeL (0.04 * fontSize) & D.fc D.black & D.font "monospace"
+                        & D.translate labelOffset
+                   in label <> origTick
+     in D.translate (D.r2 (postPos, 0)) withLabel
 
+laserline :: [D.V2 Double] -> D.Diagram D.B
 laserline positions = D.fromOffsets positions & D.lineWidth D.ultraThin & D.lc D.black
