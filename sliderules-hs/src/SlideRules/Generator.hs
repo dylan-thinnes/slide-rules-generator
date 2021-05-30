f63d5e55008d134280fef76b660fad02be69fa3e
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-05-30 14:22:57 +0100

    Add pre & post transform combinators

--- sliderules-hs/src/SlideRules/Generator.hs
+++ sliderules-hs/src/SlideRules/Generator.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE FlexibleContexts #-}
 module SlideRules.Generator where
 
 -- base
@@ -21,18 +22,18 @@ import SlideRules.Types
 import SlideRules.Utils
 
 data GenState = GenState
-    { preTrans  :: [Transformation]
-    , postTrans :: [Transformation]
-    , currTick  :: InternalFloat -> TickInfo
-    , out       :: S.Seq Tick
+    { preTransformations  :: [Transformation]
+    , postTransformations :: [Transformation]
+    , currTick            :: InternalFloat -> TickInfo
+    , out                 :: S.Seq Tick
     }
     -- deriving (Show)
 
 genTick :: InternalFloat -> GenState -> Maybe Tick
 genTick x s = do
     let info = currTick s x
-    prePos <- runTransformations (preTrans s) x
-    postPos <- runTransformations (postTrans s) prePos
+    prePos <- runTransformations (preTransformations s) x
+    postPos <- runTransformations (postTransformations s) prePos
     pure $ Tick { info, prePos, postPos }
 
 instance Default GenState where
@@ -46,8 +47,28 @@ outputAll = forever output
 output = PP.wither (gets . genTick) `for` \tick ->
     modify $ \s -> s { out = out s <> S.fromList [tick] }
 
+preTransform :: MonadState GenState m => Transformation -> m r -> m r
+preTransform transformation pipe = do
+    modify $ \s -> s { preTransformations = transformation : preTransformations s }
+    r <- pipe
+    modify $ \s -> s { preTransformations = tail $ preTransformations s }
+    pure r
+
+postTransform :: MonadState GenState m => Transformation -> m r -> m r
+postTransform transformation pipe = do
+    modify $ \s -> s { postTransformations = transformation : postTransformations s }
+    r <- pipe
+    modify $ \s -> s { postTransformations = tail $ postTransformations s }
+    pure r
+
 ex55 :: Producer InternalFloat (State GenState) ()
 ex55 = enumerate $ do
     x <- Select $ each [1..10]
     y <- Select $ each [1..x]
     return $ x * 10 + y
+
+ex100 :: Producer InternalFloat (State GenState) ()
+ex100 =
+    each [1..9]
+    `for`
+    \x -> preTransform (Offset $ fromIntegral x) $ preTransform (Scale 0.1) (each [0..9])
