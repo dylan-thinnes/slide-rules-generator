6570593051a8e039864d0f87af75c2b3c7c795af
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-05-30 17:33:41 +0100

    Add ex100 to root module

--- sliderules-hs/src/SlideRules.hs
+++ sliderules-hs/src/SlideRules.hs
@@ -1,3 +1,27 @@
 module SlideRules where
 
+-- default
+import Data.Default
+
+-- local (sliderules)
+import SlideRules.Generator
+import SlideRules.Lenses
 import SlideRules.Tick
+import SlideRules.Transformations
+import SlideRules.Types
+import SlideRules.Utils
+
+ex100 :: Generator ()
+ex100 =
+    postTransform (Log 10) $ together $
+        [ withInfo (\f x -> (f x) { mlabel = Just (def { fontSize = 10, text = showMax x }) }) $ do
+            x <- list [1..9]
+            output x
+            preTransform (Offset x) $ preTransform (Scale 0.1) $ do
+                x <- list [1..9]
+                output x
+        , withInfo (\f x -> (f x) { start = 0.5, end = 0.6, mlabel = Just (def { fontSize = 10, text = "pi" }) })
+            (output pi)
+        , withInfo (\f x -> (f x) { start = 0.5, end = 0.6, mlabel = Just (def { fontSize = 10, text = "e" }) })
+            (output e)
+        ]
