b0ff7d4d4bdbc71232dd9247db2c9e8a6532dfc1
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-06-25 21:00:08 +0100

    Add & try exact-real & fixedprec

--- src/SlideRules/Types.hs
+++ src/SlideRules/Types.hs
@@ -1,5 +1,24 @@
 {-# LANGUAGE PackageImports #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 module SlideRules.Types where
 
+import Data.CReal
+import Control.DeepSeq
+import GHC.Generics
+import Data.Number.FixedPrec
+
 type InternalFloat = Double
+
+--type InternalFloat = CReal 256
+--
+--instance NFData (CReal 256) where
+--    rnf creal = deepseq () ()
+
+--type InternalFloat = FixedPrec P10
+--
+--instance NFData (FixedPrec P10) where
+--    rnf creal = deepseq () ()
