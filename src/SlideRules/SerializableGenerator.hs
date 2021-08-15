ea739bb18e99b12bd891a57fd34acecf5a63281f
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-08-16 00:03:30 +0100

    Implement serializable versions of core sliderules library datatypes

--- src/SlideRules/SerializableGenerator.hs
+++ src/SlideRules/SerializableGenerator.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -24,32 +25,84 @@ import Data.Scientific
 -- local (sliderules)
 import SlideRules.Generator
 import SlideRules.Partitions
+import SlideRules.Renderer
+import SlideRules.Scales as Scales
 import SlideRules.Transformations
 import SlideRules.Types
 import SlideRules.Tick
 import SlideRules.Utils
+
+data SerializableTickIdentifier
+    = FloorIdentifier { leeway :: InternalFloat, lower :: Integer, upper :: Integer }
+    | DefaultIdentifier
+    deriving (Show, Generic)
+
+instance ToJSON SerializableTickIdentifier
+instance FromJSON SerializableTickIdentifier
+
+deserializeTickIdentifier :: SerializableTickIdentifier -> (InternalFloat -> [(InternalFloat, ScaleID)])
+deserializeTickIdentifier FloorIdentifier{ leeway, lower, upper } = floorIdentifier leeway (lower, upper)
+deserializeTickIdentifier DefaultIdentifier = defaultIdentifier
+
+data SerializableOffsetter
+    = SLinear { height :: InternalFloat }
+      -- ^ Hardcoded vertical offset h
+    | SIncline { intercept :: InternalFloat, slope :: InternalFloat }
+      -- ^ Incline with intercept (initial value) i, slope s
+    | SRadial { radius :: InternalFloat }
+      -- ^ Hardcoded radius r
+    | SSpiral { radius :: InternalFloat, velocity :: InternalFloat }
+      -- ^ Starting radius r, velocity v
+    deriving (Show, Generic)
+
+instance ToJSON SerializableOffsetter
+instance FromJSON SerializableOffsetter
+
+deserializeOffsetter :: SerializableOffsetter -> Offsetter
+deserializeOffsetter SLinear{ height } = linear height
+deserializeOffsetter SIncline{ intercept, slope } = incline intercept slope
+deserializeOffsetter SRadial{ radius } = radial radius
+deserializeOffsetter SSpiral{ radius, velocity } = spiral radius velocity
 
 data SerializableGenerator
     = HardcodedPoints { transformations :: [Transformation], controlPoints :: [Scientific] }
     | SmartPartitionTens { transformations :: [Transformation], controlPoints :: [Scientific] }
     deriving (Show, Generic)
 
-deriving instance Generic Transformation
-instance ToJSON Transformation
-instance FromJSON Transformation
-
 instance ToJSON SerializableGenerator
 instance FromJSON SerializableGenerator
 
-interpretSerializableGen :: SerializableGenerator -> Generator ()
-interpretSerializableGen (HardcodedPoints { transformations, controlPoints }) =
+deserializeGenerator :: SerializableGenerator -> Generator ()
+deserializeGenerator (HardcodedPoints { transformations, controlPoints }) =
     withs (postTransform <$> transformations) $ do
         traverse output $ scientificToInternalFloat <$> controlPoints
         pure ()
-interpretSerializableGen (SmartPartitionTens { transformations, controlPoints }) =
+deserializeGenerator (SmartPartitionTens { transformations, controlPoints }) =
     withs (postTransform <$> transformations) $ do
         smartPartitionTens smartHandler $ scientificToDecimal <$> controlPoints
         pure ()
+
+data SerializableScaleSpec = SerializableScaleSpec
+    { baseTolerance :: InternalFloat
+    , serializableTickIdentifier :: SerializableTickIdentifier
+    , serializableGenerator :: SerializableGenerator
+    , serializableOffsetter :: SerializableOffsetter
+    , renderSettings :: RenderSettings
+    }
+    deriving (Show, Generic)
+
+instance ToJSON SerializableScaleSpec
+instance FromJSON SerializableScaleSpec
+
+deserializeScaleSpec :: SerializableScaleSpec -> ScaleSpec
+deserializeScaleSpec SerializableScaleSpec {..} =
+    ScaleSpec
+        { baseTolerance = baseTolerance
+        , tickIdentifier = deserializeTickIdentifier serializableTickIdentifier
+        , generator = deserializeGenerator serializableGenerator
+        , offsetter = deserializeOffsetter serializableOffsetter
+        , renderSettings = renderSettings
+        }
 
 -- Partitioning handler
 smartHandler 1 = trees10
